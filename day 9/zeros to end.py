6698873c5ea949c20fa54d2bc96c787986c88284
Darshan Powar <drshnpowar@gmail.com>
2025-02-10 20:57:35 +0530

    Rename parameter 'numbers' to 'num_list' for clarity

--- day 9/zeros to end.py
+++ day 9/zeros to end.py
@@ -1,9 +1,9 @@
-def zeros_last(numbers : list):
-    numbers.sort()
-    while numbers[0]==0:
-        numbers.pop(0)
-        numbers.append(0)
-    return numbers
+def zeros_last(num_list : list):
+    num_list.sort()
+    while num_list[0]==0:
+        num_list.pop(0)
+        num_list.append(0)
+    return num_list
 
 if __name__ == "__main__":
     numbers = [2, 1, 4, 7, 6]
