1f94cab764f7e60e1196f5e3edfc924082addb8b
Darshan Powar <drshnpowar@gmail.com>
2025-03-31 20:47:13 +0530

    Day 47:Save JSON

---
+++ day 47/save a json.py
@@ -0,0 +1,26 @@
+# Day 47
+# Save JSON
+
+import json
+
+names = {'name': 'Carol', 'sex': 'female', 'age': 55}
+
+
+def save_json(dict1):
+    with open('save_json_file.json', 'w') as my_file:
+        # saving to file and adding indent
+        json.dump(dict1, my_file, indent=4)
+
+
+save_json(names)
+
+
+# Second function
+
+def read_json():
+    with open('save_json_file.json', 'r') as my_file:
+        json_file = json.load(my_file)
+    return json_file
+
+
+print(read_json())
